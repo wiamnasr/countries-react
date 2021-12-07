0505198ab99f85f94502338034c93854d9a13d57
Wiam <wiamnasr@gmail.com>
2021-12-07 13:57:42 +0000

    gitigore

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 // Components
 import CountryCard from "./components/CountryCard/CountryCard";
+import SearchBox from "./components/SearchBox/SearchBox";
 
 function App() {
   return (
     <div className='App'>
+      <SearchBox />
       <CountryCard />
     </div>
   );
---
+++ src/components/SearchBox/SearchBox.js
@@ -0,0 +1,5 @@
+const SearchBox = () => {
+  return <input type='text' />;
+};
+
+export default SearchBox;
